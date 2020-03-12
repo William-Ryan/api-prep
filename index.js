ec4f34f888de1f802f3be356c14d0fcbdc54d1cb
William <billy.ryan.rom@gmail.com>
2020-03-12 19:00:59 -0400

    Add server.js and PORT inside of index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 /* Go to https://www.youtube.com/watch?v=dQw4w9WgXcQ and follow the instructions and code! */
 
+const server = require ("./server.js")
+
+PORT = 5000
+server.listen(PORT, () => {
+    console.log(`\n* Server Running on http://localhost:${PORT} *\n`)
+})
